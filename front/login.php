32130dfa919161031ec3de5278b84e0693faa79b
yilin <sanny86827@gmail.com>
2021-07-31 18:21:50 +0800

    美化login頁面

--- front/login.php
+++ front/login.php
@@ -4,34 +4,41 @@ if(isset($_SESSION['admin'])){
 }
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
 
-<div class="di"
-    style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
+<section>
+    <div class="box">
+        
+        <div class="container">
+            <div class="form">
+                <h2>Login Form</h2>
+                <form method="post" action="../api/login.php">
+                    <!-- <p class="t botli">管理員登入區</p> -->
+                    <div class="inputBox">
+                        <input name="acc" autofocus="" type="text" placeholder="Username">
+                    </div>
+                    <div class="inputBox">
+                        <input name="pw" type="password" placeholder="Password">
+                    </div>
+                    <div class="inputBox">
+                        <input value="送出" type="submit"><input type="reset" value="清除">
+                    </div>
+                    
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
 
-    <div style="height:32px; display:block;"></div>
-    <!--正中央-->
-    <form method="post" action="../api/login.php">
-        <p class="t botli">管理員登入區</p>
-        <p class="cent">帳號 ： <input name="acc" autofocus="" type="text"></p>
-        <p class="cent">密碼 ： <input name="pw" type="password"></p>
-        <p class="cent"><input value="送出" type="submit"><input type="reset" value="清除"></p>
-    </form>
-</div>
-<div id="alt"
-    style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
-</div>
-<script>
-$(".sswww").hover(
-    function() {
-        $("#alt").html("" + $(this).children(".all").html() + "").css({
-            "top": $(this).offset().top - 50
-        })
-        $("#alt").show()
-    }
-)
-$(".sswww").mouseout(
-    function() {
-        $("#alt").hide()
-    }
-)
-</script>
+</body>
+</html>
+
